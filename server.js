50563000faadcf5664c3543cc13e24e7cc303c96
Nilesh Pant <i.nilesh.pant@redcarpetup.com>
2020-08-16 12:55:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+//const io = require('socket.io')(server)
+const server = require('http').Server(app)
+const { v4: uuidV4 } = require('uuid');
+const { response } = require('express');
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+//io.on('connection', socket =>)
+
+
+server.listen(3030)
